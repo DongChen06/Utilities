097563e150dbf48f609f018cc3b6f6347311c0f9
Dong Chen <chendondon19930606@gmail.com>
2020-09-25 23:19:29 -0400

    Add files via upload

--- npy_handler/csv_handler.py
+++ npy_handler/csv_handler.py
@@ -14,27 +14,36 @@ def readcsv(files):
     for row in plots:
         y.append((row[2]))
         x.append((row[1]))
-    return [float(i) for i in x[1:]], [float(i) * 20 for i in y[1:]]
+    return [float(i) for i in x[1:]], [float(i) for i in y[1:]]
 
 plt.figure()
-x, y = readcsv("ma2c_dial_i7/log_cnet.csv")
-plt.plot(x, y, 'g', label='CNet')
+x, y = readcsv("log_cnet.csv")
+x1, y1 = readcsv("log_dial.csv")
+# x2, y2 = readcsv("log_i5.csv")
+# x3, y3 = readcsv("log_i6.csv")
+# x4, y4 = readcsv("log_i7.csv")
+# x5, y5 = readcsv("log_i8.csv")
+x6, y6 = readcsv("log_i9.csv")
 
-x1, y1 = readcsv("ma2c_dial_i7/log_dial.csv")
-plt.plot(x1, y1, color='b', label='DIAL')
+plt.plot(x, y, label='CNet')
+plt.plot(x1, y1, label='DIAL')
+# plt.plot(x2, y2, label='I5')
+# plt.plot(x3, y3, label='I6')
+# plt.plot(x4, y4, label='I7')
+# plt.plot(x5, y5, label='I8')
+plt.plot(x6, y6, label='I9')
 
-x2, y2 = readcsv("ma2c_dial_i7/log_dial_i6.csv")
-plt.plot(x2, y2, color='red', label='I6')
+plt.xticks(fontsize=12)
+plt.yticks(fontsize=12)
 
-x3, y3 = readcsv("ma2c_dial_i7/log_i7.csv")
-plt.plot(x3, y3, color='black', label='I7')
+plt.ylim(0.35, 0.58)
+plt.xlim(0, 40000)
+plt.xlabel('Steps', fontsize=12)
+plt.ylabel('Rewards', fontsize=12)
+leg = plt.legend(fontsize=12, loc='lower right')
 
-# plt.xticks(fontsize=16)
-# plt.yticks(fontsize=16)
-
-# plt.ylim(0, 16)
-# plt.xlim(0, 104800)
-plt.xlabel('Steps', fontsize=10)
-plt.ylabel('Rewards', fontsize=10)
-plt.legend(fontsize=8)
+# set the linewidth of each legend object
+for legobj in leg.legendHandles:
+    legobj.set_linewidth(3.0)
+    
 plt.show()
